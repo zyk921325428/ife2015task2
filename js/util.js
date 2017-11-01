0ede00e93904d53417d783717dc5ce91d92240bc
zyk921325428 <921325428@qq.com>
2017-11-01 16:53:51 +0800

    deepclone done

---
+++ js/util.js
@@ -0,0 +1,44 @@
+// 判断arr是否为一个数组，返回一个bool值
+function isArray(arr) {
+return arr instanceof Array;
+}
+
+// 判断fn是否为一个函数，返回一个bool值
+function isFunction(fn) {
+    return fn instanceof Function;
+}
+
+// 使用递归来实现一个深度克隆，可以复制一个目标对象，返回一个完整拷贝
+// 被复制的对象类型会被限制为数字、字符串、布尔、日期、数组、Object对象。不会包含函数、正则对象等
+function cloneObject(src) {
+    if (typeof(src)!="object") {
+        return src;
+    }
+    var cloneObj={};
+for (var att in src) {
+        cloneObj[att] = cloneObject(src[att]);//递归
+}
+return cloneObj;
+}
+
+// 测试用例：
+var srcObj = {
+    a: 1,
+    b: {
+        b1: ["hello","hi"],
+        b2: "JavaScript",
+        b3: {www:"hello",qqq:"hi"},
+        b4:function(){}//测试
+    }
+};
+var abObj = srcObj;
+var tarObj = cloneObject(srcObj);
+
+srcObj.a = 2;
+srcObj.b.b1[0] = "Hello";
+
+console.log(abObj.a);
+console.log(abObj.b.b1[0]);
+
+console.log(tarObj.a);      // 1
+console.log(tarObj.b.b1[0]);    // "hello"
